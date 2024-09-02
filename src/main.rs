14f52344eab65eaa96be2e8e80b31cd7dc9e8515
Delamox <danieltenbosch@proton.me>
2024-09-02 18:22:17 +0000

    refactor

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,9 @@ use core::panic;
 use reqwest::Error;
 use rocket::data::{Data, ToByteUnit};
 use rocket::fs::FileServer;
-use rocket::http::uncased::AsUncased;
 use rocket::response::content::{self, RawHtml};
+
+const API_ERR: &str = "error getting API response";
 
 #[macro_use]
 extern crate rocket;
@@ -13,12 +14,11 @@ fn rocket() -> _ {
     let path = std::env::current_dir().unwrap().join("public");
     rocket::build()
         .mount("/", FileServer::from(path))
-        .mount("/", routes![search, select, index, info])
+        .mount("/", routes![info, query])
 }
 
-//respond to index query with link
-#[post("/index", data = "<data>")]
-async fn index(data: Data<'_>) -> content::RawHtml<String> {
+#[post("/query", data = "<data>")]
+async fn query(data: Data<'_>) -> content::RawHtml<String> {
     let stream: String = data
         .open(2.mebibytes())
         .into_string()
@@ -30,72 +30,65 @@ async fn index(data: Data<'_>) -> content::RawHtml<String> {
         panic!("error reading request headers")
     };
 
-    if request[0].0.as_str() == "index" {
-        let Ok(json) = searchapi(format!("watch/{}", request[0].1).as_str()).await else {
-            panic!("error getting API response")
-        };
-        let elements: String = construct_index_html(get_url(json));
-
-        RawHtml(elements)
-    } else {
-        panic!("invalid request type")
+    match request[0].0.as_str() {
+        "search" => {
+            let Ok(json) = searchapi(request[0].1.as_str()).await else {
+                panic!("{}", API_ERR)
+            };
+            RawHtml(search(json))
+        }
+        "select" => {
+            let Ok(json) = searchapi(format!("info/{}", request[0].1).as_str()).await else {
+                panic!("{}", API_ERR)
+            };
+            RawHtml(select(json))
+        }
+        "index" => {
+            let Ok(json) = searchapi(format!("watch/{}", request[0].1).as_str()).await else {
+                panic!("{}", API_ERR)
+            };
+            RawHtml(index(json))
+        }
+        _ => panic!("invalid"),
     }
 }
 
-fn get_url(json: String) -> String {
+fn index(json: String) -> String {
     let mut object: Episode = serde_json::from_str(json.as_str()).unwrap();
 
     object.sources.retain(|e| e.quality == "default");
-    match object.sources.len() {
+    let url: String = match object.sources.len() {
         1 => object.sources[0].url.clone(),
         _ => {
             object.sources.retain(|e| e.quality == "backup");
             object.sources[0].url.clone()
         }
-    }
-}
-
-fn construct_index_html(url: String) -> String {
-    let object: String = format!(
-        "<a href=\"https://haiku.leafatshredder.xyz/player/#{}\">enjoy!</a>",
-        url
-    );
-    object
-}
-
-//respond to selection with episode list
-#[post("/select", data = "<data>")]
-async fn select(data: Data<'_>) -> content::RawHtml<String> {
-    let stream: String = data
-        .open(2.mebibytes())
-        .into_string()
-        .await
-        .unwrap()
-        .into_inner()
-        .to_string();
-    let Ok(request) = serde_urlencoded::from_str::<Vec<(String, String)>>(&stream) else {
-        panic!("error reading request headers")
     };
 
-    if request[0].0.as_str() == "select" {
-        let Ok(json) = searchapi(format!("info/{}", request[0].1).as_str()).await else {
-            panic!("error getting API response")
-        };
-        let elements: String = construct_select_html(json);
-
-        RawHtml(elements)
-    } else {
-        panic!("invalid request type")
-    }
+    format!(
+        "<a href=\"https://haiku.leafatshredder.xyz/player/#{}\">enjoy!</a>",
+        url
+    )
 }
 
-fn construct_select_html(json: String) -> String {
+fn select(json: String) -> String {
     let object: Select = serde_json::from_str(json.as_str()).unwrap();
     let element = object.episodes;
 
     let mut construct: String = String::new();
     for e in element.iter() {
         construct.push_str(format!("<option value=\"{}\">{}</option>", e.id, e.number).as_str())
+    }
+    construct
+}
+
+fn search(json: String) -> String {
+    let mut object: Search = serde_json::from_str(json.as_str()).unwrap();
+    object.results.retain(|e| e.subOrDub == "sub");
+
+    let mut construct: String = String::new();
+    for e in object.results.iter() {
+        construct.push_str(format!("<option value=\"{}\">{}</option>", e.id, e.title).as_str())
     }
     construct
 }
@@ -153,7 +146,7 @@ fn construct_info_html(json: String) -> String {
         <br>status: {}</i>
     </div>
     "#,
-            object.status.as_uncased()
+            object.status.to_lowercase()
         )
         .as_str(),
     );
@@ -166,54 +159,6 @@ fn construct_info_html(json: String) -> String {
     </div>
     "#,
     );
-    construct
-}
-
-//respond to search query with possible selections
-#[post("/search", data = "<data>")]
-async fn search(data: Data<'_>) -> content::RawHtml<String> {
-    let stream: String = data
-        .open(2.mebibytes())
-        .into_string()
-        .await
-        .unwrap()
-        .into_inner()
-        .to_string();
-    let Ok(request) = serde_urlencoded::from_str::<Vec<(String, String)>>(&stream) else {
-        panic!("error reading request headers")
-    };
-
-    if request[0].0.as_str() == "searchbox" {
-        let Ok(json) = searchapi(request[0].1.as_str()).await else {
-            panic!("error getting API response")
-        };
-        let elements: String = construct_search_html(filter_sub(json));
-
-        RawHtml(elements)
-    } else {
-        panic!("invalid request type")
-    }
-}
-
-fn filter_sub(json: String) -> Vec<SearchElement> {
-    //filter dubs
-    let mut object: Search = serde_json::from_str(json.as_str()).unwrap();
-    let mut elements: Vec<SearchElement> = Vec::new();
-    object.results.retain_mut(|e| {
-        if e.subOrDub == "sub" {
-            elements.push(std::mem::take(e));
-            return false;
-        }
-        true
-    });
-    elements
-}
-
-fn construct_search_html(elements: Vec<SearchElement>) -> String {
-    let mut construct: String = String::new();
-    for e in elements.iter() {
-        construct.push_str(format!("<option value=\"{}\">{}</option>", e.id, e.title).as_str())
-    }
     construct
 }
 
